644aa27a662c375b7bb0db37b18c75746ce3dc10
Boris Raicheff <b@raicheff.com>
2019-01-08 15:15:08 +0000

    Rename project

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #
-# AppEngine-Manifold
+# AppEngine-Oil
 #
 # Copyright (C) 2017 Boris Raicheff
 # All rights reserved
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='AppEngine-Manifold',
+    name='AppEngine-Oil',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
