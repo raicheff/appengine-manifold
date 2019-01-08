1dad19dc41b4b43f6718929c17dde17638e6d273
Boris Raicheff <b@raicheff.com>
2019-01-08 13:03:51 +0000

    Hello, world!

---
+++ scripts/envinject.py
@@ -0,0 +1,34 @@
+#
+# Airpbx
+#
+# Copyright (C) 2018 Airpbx Ltd
+# All rights reserved
+#
+
+
+import os
+
+import click
+import configobj
+import oyaml
+
+
+@click.command()
+@click.argument('source', nargs=-1, type=click.File('r'))
+@click.argument('target', type=click.Path(exists=True))
+@click.option('-e', 'env_filename', type=click.Path(exists=True))
+def main(source, target, env_filename):
+    """"""
+
+    config = configobj.ConfigObj(env_filename).dict()
+
+    for src in source:
+        descriptor = oyaml.load(src)
+        env_variables = descriptor.get('env_variables') or {}
+        env_variables.update(config)
+        descriptor['env_variables'] = env_variables
+        with open(os.path.join(target, os.path.basename(src.name)), 'w') as stream:
+            oyaml.dump(descriptor, stream, default_flow_style=False)
+
+
+# EOF
---
+++ setup.py
@@ -0,0 +1,30 @@
+#
+# AppEngine-Manifold
+#
+# Copyright (C) 2017 Boris Raicheff
+# All rights reserved
+#
+
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name='AppEngine-Manifold',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=(
+        'click',
+        'configobj',
+        'oyaml',
+    ),
+    entry_points={
+        'console_scripts': (
+            'envinject = scripts.envinject:main',
+        ),
+    },
+)
+
+
+# EOF
